489a4323faef00a8c600f62c8f8c33e68d149f34
tun-lu-khant <tunlukhant1994@gmail.com>
2023-04-09 21:50:06 +0630

    Added expense model serializer and a negative test without all the require fields

--- restapi/views.py
+++ restapi/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from restapi import models
+from restapi import models, serializers
 from django.forms.models import model_to_dict
 
 
@@ -9,18 +9,11 @@ from django.forms.models import model_to_dict
 class ExpenseListCreate(APIView):
     def get(self, request):
         expenses = models.Expense.objects.all()
-
-        # I think the code should like that 'model_to_dict(expense)' while it has to be expenses
-        all_expenses = [model_to_dict(expenses) for expense in expenses]
-        return Response(all_expenses, status=200)
+        serializer = serializers.Expense(expenses, many=True)
+        return Response(serializer.data, status=200)
 
     def post(self, request):
-        amount = request.data["amount"]
-        merchant = request.data["merchant"]
-        description = request.data["description"]
-        category = request.data["category"]
-
-        expense = models.Expense.objects.create(
-            amount=amount, merchant=merchant, description=description, category=category
-        )
-        return Response(model_to_dict(expense), status=201)
+        serializer = serializers.Expense(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=201)
