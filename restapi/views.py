66f5934b8242b6065bed2f51783dccc24757aa74
tun-lu-khant <tunlukhant1994@gmail.com>
2023-04-10 00:20:10 +0630

    Added filtering support to listing view

--- restapi/views.py
+++ restapi/views.py
@@ -10,6 +10,7 @@ from rest_framework.generics import RetrieveDestroyAPIView, ListCreateAPIView
 class ExpenseListCreate(ListCreateAPIView):
     serializer_class = serializers.Expense
     queryset = models.Expense.objects.all()
+    filterset_fields = ["category", "merchant"]
 
 
 class ExpenseRetrieveDelete(RetrieveDestroyAPIView):
